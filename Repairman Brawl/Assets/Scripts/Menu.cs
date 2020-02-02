7cda2b3435fbe1e68edaac9bc9ae21440b182b51
koskurita <kkuri005@ucr.edu>
2020-02-01 17:35:56 -0800

    Updated End Screen

--- Repairman Brawl/Assets/Scripts/Menu.cs
+++ Repairman Brawl/Assets/Scripts/Menu.cs
@@ -21,6 +21,11 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     
+    public void BackToMenu(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+    
+    
     public void QuitGame(){
         Debug.Log("QUIT!");
         Application.Quit();
