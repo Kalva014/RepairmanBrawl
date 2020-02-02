8207b57bdb1d7202e451fde3e10711681bb9647c
Kalva014 <46539140+Kalva014@users.noreply.github.com>
2020-02-01 20:22:01 -0800

    Updated Status Bar

---
+++ Repairman Brawl/Assets/FillHealthBar2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillHealthBar2 : MonoBehaviour
+{
+    public GameObject player;
+    public Image fillImage;
+    private Slider slider;
+    float playerHealth;
+    float playerMaxHealth;
+    float fillValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = gameObject;
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealth = player.GetComponent<PlayerSheet2>().health;
+        playerMaxHealth = player.GetComponent<PlayerSheet2>().maxHealth;
+        fillValue = playerHealth / playerMaxHealth;
+
+        if(playerHealth <= 0) {
+            fillValue = 0;
+        }
+
+        slider.value = fillValue;
+    }
+}
---
+++ Repairman Brawl/Assets/Scripts/FillHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillHealthBar : MonoBehaviour
+{
+    public GameObject player;
+    public Image fillImage;
+    private Slider slider;
+    float playerHealth;
+    float playerMaxHealth;
+    float fillValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = gameObject;
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealth = player.GetComponent<PlayerSheet>().health;
+        playerMaxHealth = player.GetComponent<PlayerSheet>().maxHealth;
+        fillValue = playerHealth / playerMaxHealth;
+        slider.value = fillValue;
+    }
+}
