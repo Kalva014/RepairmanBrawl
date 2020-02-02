2178d846423bdf47c8bd08dcb402ab23924679dd
Kalva014 <46539140+Kalva014@users.noreply.github.com>
2020-02-02 14:30:02 -0800

    Obstacle Update

---
+++ Repairman Brawl/Assets/RandomLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLocation : MonoBehaviour
+{
+
+    public GameObject Obstacle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-8f,10f), Random.Range(-2f, 4.5f), 1);
+        transform.position = transform.position + spawnPosition;
+        Debug.Log(transform.position);
+    }
+}
---
+++ Repairman Brawl/Assets/RandomSpawnScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnScript : MonoBehaviour
+{
+    public GameObject Obstacle;
+    public float numObstacles;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        //Vector3 spawnPosition = new Vector3(Random.Range(-10f,12f), Random.Range(-4f, 6.5f), 1);
+        //transform.position = transform.position + spawnPosition;
+        //Debug.Log(transform.position);
+
+    //}
+
+    void Start()
+    {
+        //GameObject wall = Instantiate(Obstacle, transform.position, Quaternion.identity);
+
+        for (int i = 0; i < numObstacles; ++i)
+        {
+            GameObject wall = Instantiate(Obstacle, transform.position, Quaternion.identity);
+        }
+    }
+   
+}
